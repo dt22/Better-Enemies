fe38683973d6eb9960c22253d8c84713afbf92e5
dt22 <dtony22@gmail.com>
2022-04-16 13:12:14 -0500

    first change

--- Better Enemies/Class1.cs
+++ Better Enemies/Class1.cs
@@ -94,17 +94,16 @@ namespace Better_Enemies
         {
             HarmonyInstance.Create("your.mod.id").PatchAll();
             api?.Invoke("log verbose", "Mod Initialised.");
-            
-            DefRepository Repo = GameUtl.GameComponent<DefRepository>();        
+
+            DefRepository Repo = GameUtl.GameComponent<DefRepository>();
             TacticalItemDef queenSpawner = Repo.GetAllDefs<TacticalItemDef>().FirstOrDefault(a => a.name.Equals("Queen_Abdomen_Spawner_BodyPartDef"));
-            TacticalItemDef queenBelcher = Repo.GetAllDefs<TacticalItemDef>().FirstOrDefault(a => a.name.Equals("Queen_Abdomen_Belcher_BodyPartDef"));            
+            TacticalItemDef queenBelcher = Repo.GetAllDefs<TacticalItemDef>().FirstOrDefault(a => a.name.Equals("Queen_Abdomen_Belcher_BodyPartDef"));
             BodyPartAspectDef queenHeavyHead = Repo.GetAllDefs<BodyPartAspectDef>().FirstOrDefault(a => a.name.Equals("E_BodyPartAspect [Queen_Head_Heavy_BodyPartDef]"));
             BodyPartAspectDef queenSpitterHead = Repo.GetAllDefs<BodyPartAspectDef>().FirstOrDefault(a => a.name.Equals("E_BodyPartAspect [Queen_Head_Spitter_Goo_WeaponDef]"));
             BodyPartAspectDef queenSonicHead = Repo.GetAllDefs<BodyPartAspectDef>().FirstOrDefault(a => a.name.Equals("E_BodyPartAspect [Queen_Head_Sonic_WeaponDef]"));
-                                    
-            AIActionMoveAndAttackDef SirenAcidAI = Repo.GetAllDefs<AIActionMoveAndAttackDef>().FirstOrDefault(a => a.name.Equals("Siren_MoveAndSpitAcid_AIActionDef"));
+
             PsychicScreamAbilityDef sirenPsychicScream = Repo.GetAllDefs<PsychicScreamAbilityDef>().FirstOrDefault(a => a.name.Equals("Siren_PsychicScream_AbilityDef"));
-           
+
             TacticalItemDef crabmanHeavyHead = Repo.GetAllDefs<TacticalItemDef>().FirstOrDefault(a => a.name.Equals("Crabman_Head_EliteHumanoid_BodyPartDef"));
 
             WeaponDef chironBlastMortar = Repo.GetAllDefs<WeaponDef>().FirstOrDefault(a => a.name.Equals("Chiron_Abdomen_Mortar_WeaponDef"));
@@ -113,7 +112,7 @@ namespace Better_Enemies
             WeaponDef chironFireWormMortar = Repo.GetAllDefs<WeaponDef>().FirstOrDefault(a => a.name.Equals("Chiron_Abdomen_FireWorm_Launcher_WeaponDef"));
             WeaponDef chironAcidWormMortar = Repo.GetAllDefs<WeaponDef>().FirstOrDefault(a => a.name.Equals("Chiron_Abdomen_AcidWorm_Launcher_WeaponDef"));
             WeaponDef chironPoisonWormMortar = Repo.GetAllDefs<WeaponDef>().FirstOrDefault(a => a.name.Equals("Chiron_Abdomen_PoisonWorm_Launcher_WeaponDef"));
-             
+
             WeaponDef crabmanacidGrenadeAcidMortar = Repo.GetAllDefs<WeaponDef>().FirstOrDefault(a => a.name.Equals("Crabman_LeftHand_Acid_Grenade_WeaponDef"));
             WeaponDef crabmanadvancedacidGrenadeAcidMortar = Repo.GetAllDefs<WeaponDef>().FirstOrDefault(a => a.name.Equals("Crabman_LeftHand_Acid_EliteGrenade_WeaponDef"));
 
@@ -127,15 +126,15 @@ namespace Better_Enemies
 
             queenSpawner.Armor = 60;
             queenBelcher.Armor = 60;
-            
+
             queenHeavyHead.WillPower = 175;
             queenSpitterHead.WillPower = 165;
             queenSonicHead.WillPower = 170;
-            
-            SirenAcidAI.Weight = 250;
+
+
             sirenPsychicScream.ActionPointCost = 0.25f;
             sirenPsychicScream.UsesPerTurn = 1;
-            
+
             chironAcidMortar.DamagePayload.DamageKeywords[0].Value = 20;
             chironAcidMortar.ChargesMax = 18;
             crabmanacidGrenadeAcidMortar.DamagePayload.DamageKeywords[1].Value = 20; //this is second the first being the blast           
@@ -194,7 +193,46 @@ namespace Better_Enemies
             {
                 Repo.GetAllDefs<TacticalAbilityDef>().FirstOrDefault(a => a.name.Equals("CloseQuarters_AbilityDef")),
             };
-        }
+            int faceHuggerBlastDamage = 1;
+            int faceHuggerAcidDamage = 5;
+            int faceHuggerAOERadius = 2;
+
+            TacCharacterDef faceHuggerTac = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(p => p.name.Equals("Facehugger_TacCharacterDef"));
+            TacCharacterDef faceHuggerVariation = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(p => p.name.Equals("Facehugger_AlienMutationVariationDef"));
+
+            string skillName = "BC_SwarmerAcidExplosion_Die_AbilityDef";
+            RagdollDieAbilityDef source = Repo.GetAllDefs<RagdollDieAbilityDef>().FirstOrDefault(p => p.name.Equals("SwarmerAcidExplosion_Die_AbilityDef"));
+            RagdollDieAbilityDef sAE = Clone.CreateDefFromClone(
+                source,
+                "1137345a-a18d-4800-b52e-b15d49f4dabf",
+                skillName);
+            sAE.ViewElementDef = Clone.CreateDefFromClone(
+                source.ViewElementDef,
+                "10729876-f764-41b5-9b4e-c8cb98dca771",
+                skillName);
+            DamagePayloadEffectDef sAEEffect = Clone.CreateDefFromClone(
+                Repo.GetAllDefs<DamagePayloadEffectDef>().FirstOrDefault(p => p.name.Equals("E_Element0 [SwarmerAcidExplosion_Die_AbilityDef]")),
+                "ac9cd527-72d4-42d2-af32-5efbdf32812e",
+                "E_Element0 [BC_SwarmerAcidExplosion_Die_AbilityDef]");
+
+            sAE.DeathEffect = sAEEffect;
+            sAEEffect.DamagePayload.DamageKeywords[0].Value = faceHuggerBlastDamage;
+            sAEEffect.DamagePayload.DamageKeywords[1].Value = faceHuggerAcidDamage;
+            sAEEffect.DamagePayload.AoeRadius = faceHuggerAOERadius;
+
+            sAE.ViewElementDef.DisplayName1 = new LocalizedTextBind("ACID EXPLOSION");
+            sAE.ViewElementDef.Description = new LocalizedTextBind("Upon death, the mind fragger bursts in an acid explosion damaging nearby targets");
+
+            faceHuggerTac.Data.Abilites = new TacticalAbilityDef[]
+            {
+                sAE,
+            };
+            faceHuggerVariation.Data.Abilites = new TacticalAbilityDef[]
+            {
+                sAE,
+            };
+
+    }
         public static void MainMod(Func<string, object, object> api)
         {
             HarmonyInstance.Create("your.mod.id").PatchAll();
