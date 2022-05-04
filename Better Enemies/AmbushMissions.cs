0151f99168b6d00b6e930402a7fa5c66eba75a17
dt22 <dtony22@gmail.com>
2022-05-04 17:10:27 -0500

    Ambush Missions enemy goes first

---
+++ Better Enemies/AmbushMissions.cs
@@ -0,0 +1,20 @@
+﻿using Base.Core;
+using Base.Defs;
+using PhoenixPoint.Common.Levels.Missions;
+using System.Linq;
+
+namespace Better_Enemies
+{
+    internal class AmbushMissions
+    {
+        public static void Change_Ambush()
+        {
+            DefRepository Repo = GameUtl.GameComponent<DefRepository>();
+
+            TacMissionDef ambush = Repo.GetAllDefs<TacMissionDef>().FirstOrDefault(a => a.name.Equals("Ambush_TacMissionDef"));
+
+            ambush.MissionData.MissionType.ParticipantsRelations[0].FirstParticipant = TacMissionParticipant.Intruder;
+            ambush.MissionData.MissionType.ParticipantsRelations[1].FirstParticipant = TacMissionParticipant.Player;
+        }
+    }
+}
