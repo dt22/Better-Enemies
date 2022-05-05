5abb902048b249375185d86d8cfd36b1f7092404
dt22 <dtony22@gmail.com>
2022-05-05 10:57:53 -0500

    Temporarily disabled Ambush changes

--- Better Enemies/AmbushMissions.cs
+++ Better Enemies/AmbushMissions.cs
@@ -10,21 +10,22 @@ namespace Better_Enemies
         public static void Change_Ambush()
         {
             DefRepository Repo = GameUtl.GameComponent<DefRepository>();
-
+            /*
             TacMissionDef ambush = Repo.GetAllDefs<TacMissionDef>().FirstOrDefault(a => a.name.Equals("Ambush_TacMissionDef"));
 
             ambush.MissionData.MissionType.ParticipantsRelations[0].FirstParticipant = TacMissionParticipant.Intruder;
             ambush.MissionData.MissionType.ParticipantsRelations[0].SecondParticipant = TacMissionParticipant.Player;
 
-          // ambush.MissionData.MissionType.DefaultDeploymentRules.OrderOfDeployment[0] = TacMissionParticipant.Intruder;
-          // ambush.MissionData.MissionType.DefaultDeploymentRules.OrderOfDeployment[1] = TacMissionParticipant.Player;
-          //
-          // ambush.MissionData.MissionParticipants = new System.Collections.Generic.List<TacMissionFactionData>
-          // {
-          //     ambush.MissionData.MissionParticipants[1],
-          //     ambush.MissionData.MissionParticipants[2],
-          //     ambush.MissionData.MissionParticipants[0],
-          // };
+            ambush.MissionData.MissionType.DefaultDeploymentRules.OrderOfDeployment[0] = TacMissionParticipant.Intruder;
+            ambush.MissionData.MissionType.DefaultDeploymentRules.OrderOfDeployment[1] = TacMissionParticipant.Player;
+          
+            ambush.MissionData.MissionParticipants = new System.Collections.Generic.List<TacMissionFactionData>
+            {
+               ambush.MissionData.MissionParticipants[1],
+               ambush.MissionData.MissionParticipants[2],
+               ambush.MissionData.MissionParticipants[0],
+            };
+            */
         }
     }
 }
