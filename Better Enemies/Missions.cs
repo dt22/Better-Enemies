cdc5363a5c8f2213011710efba1511e363f1ba2b
dt22 <dtony22@gmail.com>
2022-06-09 00:01:29 -0500

    Alerted enemies

---
+++ Better Enemies/Missions.cs
@@ -0,0 +1,30 @@
+﻿using Base;
+using Base.Core;
+using Base.Defs;
+using PhoenixPoint.Common.Core;
+using PhoenixPoint.Common.Entities.GameTags;
+using PhoenixPoint.Common.Entities.Items;
+using PhoenixPoint.Common.Levels.Missions;
+using PhoenixPoint.Geoscape.Levels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Better_Enemies
+{
+    internal class Missions
+    {
+        public static void Change_Ambush()
+        {
+            DefRepository Repo = GameUtl.GameComponent<DefRepository>();
+
+            CustomMissionTypeDef px14 = Repo.GetAllDefs<CustomMissionTypeDef>().FirstOrDefault(a => a.name.Equals("StoryPX14_CustomMissionTypeDef"));
+            CustomMissionTypeDef px1 = Repo.GetAllDefs<CustomMissionTypeDef>().FirstOrDefault(a => a.name.Equals("StoryPX1_CustomMissionTypeDef"));
+            CustomMissionTypeDef px15 = Repo.GetAllDefs<CustomMissionTypeDef>().FirstOrDefault(a => a.name.Equals("StoryPX15_CustomMissionTypeDef"));
+
+            px14.IsAiAlertedInitially = true;
+            px1.IsAiAlertedInitially = true;
+            px15.IsAiAlertedInitially = true;
+        }
+    }       
+}
