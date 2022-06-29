92e8089a38bae7a105b48c1bf87ec18ed8adc643
dt22 <dtony22@gmail.com>
2022-06-28 20:49:23 -0500

    c

--- Better Enemies/Missions.cs
+++ Better Enemies/Missions.cs
@@ -4,6 +4,7 @@ using Base.Defs;
 using PhoenixPoint.Common.Core;
 using PhoenixPoint.Common.Entities.GameTags;
 using PhoenixPoint.Common.Entities.Items;
+using PhoenixPoint.Common.Levels.ActorDeployment;
 using PhoenixPoint.Common.Levels.Missions;
 using PhoenixPoint.Geoscape.Levels;
 using PhoenixPoint.Tactical.Entities;
@@ -26,6 +27,12 @@ namespace Better_Enemies
             ApplyStatusAbilityDef coCorruption = Repo.GetAllDefs<ApplyStatusAbilityDef>().FirstOrDefault(a => a.name.Equals("Acheron_CoCorruption_AbilityDef"));
             TacCharacterDef pool = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(a => a.name.Equals("SpawningPoolCrabman_TacCharacterDef"));
             TacCharacterDef node = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(a => a.name.Equals("CorruptionNode_TacCharacterDef"));
+            TacMissionDef fourthIni = Repo.GetAllDefs<TacMissionDef>().FirstOrDefault(a => a.name.Equals("FewAliensAttack_Haven_TacMissionDef"));
+            TacMissionDef newNestTest = Repo.GetAllDefs<TacMissionDef>().FirstOrDefault(a => a.name.Equals("AlienNest_NJ_Attack_TacMissionDef"));
+            TacCharacterDef njass3 = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(a => a.name.Equals("NJ_Assault3_CharacterTemplateDef"));
+            TacCharacterDef njheavy3 = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(a => a.name.Equals("NJ_Heavy3_CharacterTemplateDef"));
+            TacCharacterDef njsniper3 = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(a => a.name.Equals("NJ_Sniper3_CharacterTemplateDef"));
+            TacCharacterDef njtech3 = Repo.GetAllDefs<TacCharacterDef>().FirstOrDefault(a => a.name.Equals("NJ_Technician3_CharacterTemplateDef"));
 
             pool.Data.Abilites = new TacticalAbilityDef[]
             {
@@ -40,6 +47,19 @@ namespace Better_Enemies
             px14.IsAiAlertedInitially = true;
             px1.IsAiAlertedInitially = true;
             px15.IsAiAlertedInitially = true;
+
+            //fourthIni.MissionData.MissionParticipants[3].ActorDeployData = new List<ActorDeployData>
+            //{
+            //    newNestTest.MissionData.MissionParticipants[3].ActorDeployData[0],
+            //    newNestTest.MissionData.MissionParticipants[3].ActorDeployData[0],
+            //    newNestTest.MissionData.MissionParticipants[3].ActorDeployData[0],
+            //    newNestTest.MissionData.MissionParticipants[3].ActorDeployData[0],
+            //};
+
+            fourthIni.MissionData.MissionParticipants[0].ActorDeployData[0].InstanceDef = njtech3;
+            fourthIni.MissionData.MissionParticipants[0].ActorDeployData[1].InstanceDef = njass3;
+            fourthIni.MissionData.MissionParticipants[0].ActorDeployData[2].InstanceDef = njheavy3;
+            fourthIni.MissionData.MissionParticipants[0].ActorDeployData[3].InstanceDef = njsniper3;
         }
     }       
 }
